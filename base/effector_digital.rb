04b2e1f0c8fea324c67072681a45d490f4b3e08b
EssentialAccident <linooropeza@gmail.com>
2020-06-03 19:59:07 -0400

    Add and test EffectorDigital class

--- base/effector_digital.rb
+++ base/effector_digital.rb
@@ -20,4 +20,46 @@ class EffectorDigital < Effector
     end
     @pin = pins.values[0] # default pin
   end
+
+  # State related methods
+  def state(pin = @pin)
+    @states[pin_number(pin)]
+  end
+
+  def on?(pin = @pin)
+    @states[pin_number(pin)] == :high
+  end
+
+  def off?(pin = @pin)
+    @states[pin_number(pin)] == :low
+  end
+
+  # Change state methods
+  def turn_on(pin = @pin)
+    RPi::GPIO.set_high pin_number(pin)
+    @states[pin_number(pin)] = :high
+  end
+
+  def turn_off(pin = @pin)
+    RPi::GPIO.set_low pin_number(pin)
+    @states[pin_number(pin)] = :low
+  end
+
+  def switch(pin = @pin)
+    if on?(pin)
+      turn_off(pin)
+    else
+      turn_on(pin)
+    end
+  end
+
+  private
+
+  def pin_number(pin)
+    if pin.class.name == 'Integer' # by number
+      pin
+    elsif @pins[pin] # by name
+      @pins[pin]
+    end
+  end
 end
--- spec/base/effector_digital_spec.rb
+++ spec/base/effector_digital_spec.rb
@@ -2,23 +2,78 @@
 
 require_relative '../../base/effector_digital'
 
-describe EffectorDigital, 'initialization' do
-  subject(:pins) { { r: 6, g: 13, b: 19 } }
+describe EffectorDigital do
+  describe 'when first created' do
+    subject(:pins) { { r: 6, g: 13, b: 19 } }
 
-  it 'initializes the pins and sets them low' do
-    effector = EffectorDigital.new pins, :low
+    it 'initializes the pins and sets them low' do
+      effector = EffectorDigital.new pins, :low
 
-    expect(effector.states). to eq({ 6 => :low, 13 => :low, 19 => :low })
+      expect(effector.states). to eq({ 6 => :low, 13 => :low, 19 => :low })
+    end
+
+    it 'initializes the pins and sets them high' do
+      effector = EffectorDigital.new pins, :high
+
+      expect(effector.states). to eq({ 6 => :high, 13 => :high, 19 => :high })
+    end
+
+    it 'initializes the pins as low by default' do
+      effector = EffectorDigital.new pins
+
+      expect(effector.states). to eq({ 6 => :low, 13 => :low, 19 => :low })
+    end
   end
-  it 'initializes the pins and sets them high' do
-    effector = EffectorDigital.new pins, :high
 
-    expect(effector.states). to eq({ 6 => :high, 13 => :high, 19 => :high })
+  describe 'describe states' do
+    subject(:effector) { EffectorDigital.new({ r: 6, g: 13, b: 19 }) }
+
+    it 'returns the state of one of the pins by its name' do
+      expect(effector.state(:r)).to eq(:low)
+    end
+    it 'returns the state of one of the pins by its number' do
+      expect(effector.state(6)).to eq(:low)
+    end
+    it 'checks logic depending on and off when on' do
+      effector.turn_on(:b)
+      expect(effector.on?(:b)).to eq(true)
+      expect(effector.off?(:b)).to eq(false)
+    end
+    it 'checks logic depending on and off when off' do
+      effector.turn_off(:b)
+      expect(effector.on?(:b)).to eq(false)
+      expect(effector.off?(:b)).to eq(true)
+    end
   end
 
-  it 'initializes the pins as low by default' do
-    effector = EffectorDigital.new pins
+  describe 'change state' do
+    subject(:effector) { EffectorDigital.new({ r: 6, g: 13, b: 19 }) }
 
-    expect(effector.states). to eq({ 6 => :low, 13 => :low, 19 => :low })
+    it 'should turn the pin on by name' do
+      effector.turn_on(:r)
+      expect(effector.state(:r)).to eq(:high)
+    end
+    it 'should turn the pin on by number' do
+      effector.turn_on(13)
+      expect(effector.state(13)).to eq(:high)
+    end
+    it 'should turn the pin off by name' do
+      effector.turn_off(:r)
+      expect(effector.state(:r)).to eq(:low)
+    end
+    it 'should turn the pin on by number' do
+      effector.turn_off(13)
+      expect(effector.state(13)).to eq(:low)
+    end
+    it 'should switch from on to off' do
+      effector.turn_on(:r)
+      effector.switch(:r)
+      expect(effector.off?(:r)).to eq(true)
+    end
+    it 'should switch from off to on' do
+      effector.turn_off(:r)
+      effector.switch(:r)
+      expect(effector.on?(:r)).to eq(true)
+    end
   end
 end
